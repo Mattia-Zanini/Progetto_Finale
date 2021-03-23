4497c85a0302f891b5818a873c7135ce607b99c3
Mattia-Zanini <mattzanini2@gmail.com>
2021-03-23 22:13:41 +0100

    Create Form1.cs

---
+++ Hyper Battleship/Hyper Battleship/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hyper_Battleship
+{
+    public partial class Gioco : Form
+    {
+        public Gioco()
+        {
+            InitializeComponent();
+        }
+
+        private void Hyper_Battleship_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Gioco_Deactivate(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
