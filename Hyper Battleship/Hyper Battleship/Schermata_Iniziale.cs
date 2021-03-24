c8577b15889978b3398bf2bf849f6cdb6c014239
Mattia-Zanini <mattzanini2@gmail.com>
2021-03-24 12:35:29 +0100

    Update Schermata_Iniziale.cs

--- Hyper Battleship/Hyper Battleship/Schermata_Iniziale.cs
+++ Hyper Battleship/Hyper Battleship/Schermata_Iniziale.cs
@@ -19,7 +19,8 @@ namespace Hyper_Battleship
 
         private void Schermata_Iniziale_Load(object sender, EventArgs e)
         {
-            
+            Gioco game = new Gioco();
+            game.chiusuraGiocoInnavvertita = true;//per ripristinare l'allert
         }
 
         private void quitText_MouseClick(object sender, MouseEventArgs e)
@@ -29,20 +30,7 @@ namespace Hyper_Battleship
 
         private void startText_MouseClick(object sender, MouseEventArgs e)
         {
-            //// Ottiene la posizione del form
-            //Schermata_Iniziale prova = new Schermata_Iniziale();
-            //Point schermataIniziale_pt = new Point(prova.Left - 10, prova.Top - 30); //il "-10"e il "- 30" servono per rendere a livello estetico più precisa l'apparizione dell'altro form
-
-            //// Translate into the screen coordinate system.
-            //Point screen_pt = this.PointToScreen(schermataIniziale_pt);//converte il valore della posizione della finestra, in coordinate nello schermo del computer
-
-            //Gioco game = new Gioco();//creo l'oggetto
-            //game.Location = screen_pt;//assegna la posizione dell'attuale form a quello "Gioco"
-            //game.Show();//mostra l'altro form
-            //this.Hide();//nasconde l'attuale form
-
             int y = schermataPreLobby.Location.Y;
-            int y2 = backButton.Location.Y;
 
             backButton.Visible = true;
             fastMatchButton.Visible = true;
@@ -53,18 +41,83 @@ namespace Hyper_Battleship
             quitText.Visible = false;
         }
 
+        public bool sezione = false; //true = seconda sezione, ovvero schermata di scelta tra singleplayer e multiplayer
+                                     //false = prima sezione, ovvero schermata di scelta delle modalità di gioco
         private void backButton_Click(object sender, EventArgs e)
         {
             int y = schermataPreLobby.Location.Y;
             int y2 = backButton.Location.Y;
 
-            backButton.Visible = false;
+            if(sezione == false)
+            {
+                backButton.Visible = false;
+                fastMatchButton.Visible = false;
+                fullMatchButton.Visible = false;
+                schermataPreLobby.Location = new Point(1265, y);
+                startText.Visible = true;
+                settingsText.Visible = true;
+                quitText.Visible = true;
+            }
+            else
+            {
+                fastMatchButton.Visible = true;
+                fullMatchButton.Visible = true;
+                singlePlayerButton.Visible = false;
+                multiplayerButton.Visible = false;
+                sezione = false;
+            }
+        }
+
+        public bool modalità = false; //false = partita veloce
+                                      //true = partita normale
+        private void fastMatchButton_Click(object sender, EventArgs e)
+        {
+            sezione = true;
             fastMatchButton.Visible = false;
             fullMatchButton.Visible = false;
-            schermataPreLobby.Location = new Point(1265, y);
-            startText.Visible = true;
-            settingsText.Visible = true;
-            quitText.Visible = true;
+            singlePlayerButton.Visible = true;
+            multiplayerButton.Visible = true;
+        }
+
+        private void fullMatchButton_Click(object sender, EventArgs e)
+        {
+            sezione = true;
+            fastMatchButton.Visible = false;
+            fullMatchButton.Visible = false;
+            singlePlayerButton.Visible = true;
+            multiplayerButton.Visible = true;
+            modalità = true;
+        }
+
+        public bool multigiocatore = false; //false = giocatore singolo
+                                            //true = multigiocatore
+        private void singlePlayerButton_Click(object sender, EventArgs e)
+        {
+            //ottiene la posizione del form
+            Schermata_Iniziale schInizialeLocationPoint_pt = new Schermata_Iniziale();
+            Point schermataIniziale_pt = new Point(schInizialeLocationPoint_pt.Left - 10, schInizialeLocationPoint_pt.Top - 30); //il "-10"e il "- 30" servono per rendere a livello estetico più precisa l'apparizione dell'altro form
+
+            //traduce la posizione della finestra in coordinate dello schermo
+            Point schInizialeLocationScreen_pt = this.PointToScreen(schermataIniziale_pt);//converte il valore della posizione della finestra, in coordinate nello schermo del computer
+
+            Gioco game = new Gioco();//creo l'oggetto
+            game.Location = schInizialeLocationScreen_pt;//assegna la posizione dell'attuale form a quello "Gioco"
+            game.Show();//mostra l'altro form
+            this.Hide();//nasconde l'attuale form
+        }
+
+        private void multiplayerButton_Click(object sender, EventArgs e)
+        {
+            multigiocatore = true;
+            Schermata_Iniziale schInizialeLocationPoint_pt = new Schermata_Iniziale();
+            Point schermataIniziale_pt = new Point(schInizialeLocationPoint_pt.Left - 10, schInizialeLocationPoint_pt.Top - 30);
+
+            Point schInizialeLocationScreen_pt = this.PointToScreen(schermataIniziale_pt);
+
+            Gioco game = new Gioco();
+            game.Location = schInizialeLocationScreen_pt;
+            game.Show();
+            this.Hide();
         }
     }
 }
