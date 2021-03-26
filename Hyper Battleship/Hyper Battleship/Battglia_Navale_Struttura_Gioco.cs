e13627d5933d3b4c388f1b2d21a17bbea3c7db61
Mattia-Zanini <mattzanini2@gmail.com>
2021-03-26 09:34:49 +0100

    Create Battglia_Navale_Struttura_Gioco.cs

---
+++ Hyper Battleship/Hyper Battleship/Battglia_Navale_Struttura_Gioco.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hyper_Battleship
+{
+    class Battglia_Navale_Struttura_Gioco
+    {
+        //Griglia 10 x 10 caselle. partita normale
+        public static string[] StrutturaGriglia10 = new string[]  {"acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no"};
+
+        //Griglia 6 x 6 caselle, partita veloce
+        public static string[] StrutturaGriglia6 = new string[]   {"acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
+                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no"};
+
+        public static bool finePartita = false;
+        public void Gioco()
+        {
+            do
+            {
+                //controllo se l'utente/utenti ha/hanno scelto una partita normale o veloce
+
+            } while (finePartita == false);
+        }
+    }
+}
