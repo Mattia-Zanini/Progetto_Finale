693797cbd5f1a047df5d987466d04ff6807600b9
Mattia-Zanini <mattzanini2@gmail.com>
2021-03-29 17:47:12 +0200

    Update Battglia_Navale_Struttura_Gioco.cs

--- Hyper Battleship/Hyper Battleship/Battglia_Navale_Struttura_Gioco.cs
+++ Hyper Battleship/Hyper Battleship/Battglia_Navale_Struttura_Gioco.cs
@@ -9,31 +9,46 @@ namespace Hyper_Battleship
     class Battglia_Navale_Struttura_Gioco
     {
         //Griglia 10 x 10 caselle. partita normale
-        public static string[] StrutturaGriglia10 = new string[]  {"acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no"};
-
+        public static string[] StrutturaGriglia10 = new string[]  {"acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no" };
         //Griglia 6 x 6 caselle, partita veloce
-        public static string[] StrutturaGriglia6 = new string[]   {"acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no",
-                                                                   "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no", "acqua,G1_no,G2_no"};
+        public static string[] StrutturaGriglia6 = new string[]   {"acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
+                                                                   "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no" };
 
         public static bool finePartita = false;
-        public void Gioco()
+        public static int scoreGiocatore1 = 0, scoreGiocatore2 = 0;
+        public void Gioco(bool modalità)
         {
+            Schermata_Iniziale f1 = new Schermata_Iniziale();
+            byte quantitàRadar = 1, quantitàDoppioAssalto;//booster partita
+            if(modalità == false)//controllo se l'utente/utenti ha/hanno scelto una partita normale o veloce
+            {
+                quantitàDoppioAssalto = 1;
+            }
+            else
+            {
+                quantitàDoppioAssalto = 2;
+            }
+
+            //Giocatore 1 dispone le navi
+
+
+
+
             do
             {
-                //controllo se l'utente/utenti ha/hanno scelto una partita normale o veloce
 
             } while (finePartita == false);
         }
