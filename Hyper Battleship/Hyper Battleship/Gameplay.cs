45384f7f2c44e16b98d8cfeb143ca14f2f2ec86d
Mattia-Zanini <mattzanini2@gmail.com>
2021-04-02 21:00:43 +0200

    Update Gameplay.cs
    aggiunta disposizione iniziale delle navi (primi script)

--- Hyper Battleship/Hyper Battleship/Gameplay.cs
+++ Hyper Battleship/Hyper Battleship/Gameplay.cs
@@ -3,11 +3,12 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Imaging;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
-
 namespace Hyper_Battleship
 {
     public partial class Gameplay : Form
@@ -15,6 +16,7 @@ namespace Hyper_Battleship
         public Gameplay()
         {
             InitializeComponent();
+            //Gioco();
         }
 
         //Griglia 10 x 10 caselle. partita normale
@@ -36,30 +38,222 @@ namespace Hyper_Battleship
                                                                    "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no",
                                                                    "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no", "acqua1,acqua2,G1_no,G2_no" };
 
-        public static bool finePartita = false;
+        public static bool finePartita = false, disposizioneNaviPossibile = true;
         public static int scoreGiocatore1 = 0, scoreGiocatore2 = 0;
-        public void Gioco(bool modalità)
+
+        public bool confirmButtonePressed = false;
+        private void confermaButton_Click(object sender, EventArgs e)
+        {
+            confirmButtonePressed = true;
+        }
+
+        string nave = "";
+        bool moveUp, moveDown, moveLeft, moveRight, rotateLeft, rotateRight;
+        int giro;
+        private void moveShipEvent(object sender, EventArgs e)
+        {
+            switch (nave)
+            {
+                case "portaerei":
+                    if (moveUp == true && portaereiGriglia10x10.Top > 40 && confirmButtonePressed == false)
+                    {
+                        portaereiGriglia10x10.Top -= 63;
+                    }
+                    if (moveDown == true && portaereiGriglia10x10.Top < 607 && confirmButtonePressed == false)
+                    {
+                        portaereiGriglia10x10.Top += 63;
+                    }
+                    if (moveRight == true && portaereiGriglia10x10.Right < 675 && confirmButtonePressed == false)
+                    {
+                        portaereiGriglia10x10.Left += 63;
+                    }
+                    if (moveLeft == true && portaereiGriglia10x10.Left > 45 && confirmButtonePressed == false)
+                    {
+                        portaereiGriglia10x10.Left -= 63;
+                    }
+                    if (rotateLeft == true && confirmButtonePressed == false)
+                    {
+                        Bitmap bitmap = (Bitmap)portaereiGriglia10x10.Image;
+                        portaereiGriglia10x10.Image = (Image)(RotateImg(bitmap, 90.0f));
+                        giro += 90;
+                        if(giro == 360)
+                        {
+                            giro = 0;
+                        }
+                        switch (giro)
+                        {
+                            case 0:
+                                portaereiGriglia10x10.Width = 315; portaereiGriglia10x10.Height = 63;break;
+                            case 90:
+                                portaereiGriglia10x10.Width = 63; portaereiGriglia10x10.Height = 315; break;
+                            case 180:
+                                portaereiGriglia10x10.Width = 315; portaereiGriglia10x10.Height = 63; break;
+                            case 270:
+                                portaereiGriglia10x10.Width = 63; portaereiGriglia10x10.Height = 315; break;
+                        }
+                    }
+                    if (rotateRight == true && confirmButtonePressed == false)
+                    {
+                        Bitmap bitmap = (Bitmap)portaereiGriglia10x10.Image;
+                        portaereiGriglia10x10.Image = (Image)(RotateImg(bitmap, -90.0f));
+                        giro -= 90;
+                        if (giro == -360)
+                        {
+                            giro = 0;
+                        }
+                        switch (giro)
+                        {
+                            case 0:
+                                portaereiGriglia10x10.Width = 315; portaereiGriglia10x10.Height = 63; break;
+                            case -90:
+                                portaereiGriglia10x10.Width = 63; portaereiGriglia10x10.Height = 315; break;
+                            case -180:
+                                portaereiGriglia10x10.Width = 315; portaereiGriglia10x10.Height = 63; break;
+                            case -270:
+                                portaereiGriglia10x10.Width = 63; portaereiGriglia10x10.Height = 315; break;
+                        }
+                    }
+                    break;
+            }
+        }
+
+        private void Gameplay_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                moveUp = true;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                moveDown = true;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                moveLeft = true;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                moveRight = true;
+            }
+            if (e.KeyCode == Keys.E)
+            {
+                rotateRight = true;
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                rotateLeft = true;
+            }
+        }
+
+        private void Gameplay_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.W)
+            {
+                moveUp = false;
+            }
+            if (e.KeyCode == Keys.S)
+            {
+                moveDown = false;
+            }
+            if (e.KeyCode == Keys.A)
+            {
+                moveLeft = false;
+            }
+            if (e.KeyCode == Keys.D)
+            {
+                moveRight = false;
+            }
+            if (e.KeyCode == Keys.E)
+            {
+                rotateRight = false;
+            }
+            if (e.KeyCode == Keys.Q)
+            {
+                rotateLeft = false;
+            }
+        }
+
+        private void portaereiPictureBox10x10_Click(object sender, EventArgs e)
+        {
+            if (disposizioneNaviPossibile)
+            {
+                nave = "portaerei";
+                disposizioneNaviPossibile = false;
+                portaereiGriglia10x10.Location = new Point(297 - 63*2, 292); //297, 292
+                portaereiGriglia10x10.Visible = true;
+            }
+        }
+
+        public void Gioco()
         {
             int turno = 0;
             Schermata_Iniziale f1 = new Schermata_Iniziale();
             byte quantitàRadar = 1, quantitàDoppioAssalto;//booster partita
-            if (modalità == false)//controllo se l'utente/utenti ha/hanno scelto una partita normale o veloce
+            if (f1.modalitàAssign() == false)//controllo se l'utente/utenti ha/hanno scelto una partita normale o veloce
             {
-                quantitàDoppioAssalto = 1;
+                quantitàDoppioAssalto = 1;//veloce
             }
             else
             {
-                quantitàDoppioAssalto = 2;
+                quantitàDoppioAssalto = 2;//normale
             }
-
-            //Giocatore 1 dispone le navi
-            //Giocatore 2 dispone le navi
 
             do
             {
                 turno++;
+                if(turno > 100)
+                {
+                    break;
+                }
 
             } while (finePartita == false);
         }
+
+        public static Bitmap RotateImg(Bitmap bmp, float angle)
+
+        {
+
+            int w = bmp.Width;
+
+            int h = bmp.Height;
+
+            Bitmap tempImg = new Bitmap(w, h);
+
+            Graphics g = Graphics.FromImage(tempImg);
+
+            g.DrawImageUnscaled(bmp, 1, 1);
+
+            g.Dispose();
+
+            GraphicsPath path = new GraphicsPath();
+
+            path.AddRectangle(new RectangleF(0f, 0f, w, h));
+
+            Matrix mtrx = new Matrix();
+
+            mtrx.Rotate(angle);
+
+            RectangleF rct = path.GetBounds(mtrx);
+
+            Bitmap newImg = new Bitmap(Convert.ToInt32(rct.Width), Convert.ToInt32(rct.Height));
+
+            g = Graphics.FromImage(newImg);
+
+            g.TranslateTransform(-rct.X, -rct.Y);
+
+            g.RotateTransform(angle);
+
+            g.InterpolationMode = InterpolationMode.HighQualityBilinear;
+
+            g.DrawImageUnscaled(tempImg, 0, 0);
+
+            g.Dispose();
+
+            tempImg.Dispose();
+
+            return newImg;
+
+        }
+
     }
 }
